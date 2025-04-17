8f75557d3341497112cada2dcc8579e9bcb4e5cc
xSStephen <pufuletidinamici@gmail.com>
2025-04-18 01:06:19 +0300

    a bug for tommorow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,14 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "ro.devnautica.quizapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -41,4 +43,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
---
+++ lib/routes.dart
@@ -0,0 +1,18 @@
+
+import 'package:quizapp/about/about.dart';
+import 'package:quizapp/home/home.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:quizapp/profile/profile.dart';
+import 'package:quizapp/topics/topics.dart';
+
+var appRoutes = {
+  '/': (context) => const HomePage(),
+  '/login': (context) => const LoginPage(),
+  '/topics': (context) => const TopicsPage(),
+  '/profile': (context) => const ProfilePage(),
+  '/about': (context) => const AboutPage(),
+};
+
+
+
+
