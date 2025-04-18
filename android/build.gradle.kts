bf9441e46fcea39a20554f1fb9c86d0df6c7dc18
xSStephen <pufuletidinamici@gmail.com>
2025-04-19 00:43:11 +0300

    Firebase finally implemented

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,5 +23,6 @@ tasks.register<Delete>("clean") {
 plugins {
   // Add the dependency for the Google services Gradle plugin
   id("com.google.gms.google-services") version "4.4.2" apply false
+  
 
 }
