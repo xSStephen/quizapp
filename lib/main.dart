b441270605206df4fe5f3e026c5e8d996b291662
xSStephen <pufuletidinamici@gmail.com>
2025-05-01 20:00:46 +0300

    fix a runtime error bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,24 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quizapp/routes.dart';
+import 'package:quizapp/services/services.dart';
+import 'package:quizapp/shared/shared.dart';
 import 'package:quizapp/theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(const App());
 }
 
-/// We are using a StatefulWidget such that we only create the [Future] once,
-/// no matter how many times our widget rebuild.
-/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
-/// would re-initialize FlutterFire and make our application re-enter loading state,
-/// which is undesired.
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
 
   @override
-  State<MyApp> createState() => _AppState();
+  State<App> createState() => _AppState();
 }
 
-class _AppState extends State<MyApp> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
+class _AppState extends State<App> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
@@ -33,22 +29,25 @@ class _AppState extends State<MyApp> {
       builder: (context, snapshot) {
         // Check for errors
         if (snapshot.hasError) {
-          return Directionality(
-            textDirection: TextDirection.ltr,
-            child: Text('error'),
-          );
+          // Error screen
         }
 
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(routes: appRoutes, theme: appTheme);
+          return StreamProvider(
+            create: (_) => FirestoreService().streamReport(),
+            catchError: (_, err) => Report(),
+            initialData: Report(),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: true,
+              routes: appRoutes,
+              theme: appTheme,
+            ),
+          );
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
-        return Directionality(
-          textDirection: TextDirection.ltr,
-          child: Text('loading'),
-        );
+        return const MaterialApp(home: LoadingScreen());
       },
     );
   }
